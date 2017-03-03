11338d36baf0eb24b504aa754eaaea27a4f7158f
Taoukilite <taoukilite.elmahdi@gmail.com>
2017-03-03 22:01:09 +0100

    amqp

--- routes/calcpanel.js
+++ routes/calcpanel.js
@@ -6,36 +6,38 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('calcpanel');
 });
+//TODO generating tickets
 router.post('/',function (req, res) {
     if(req.body.code === undefined || req.body.code===''){
         res.redirect('calcpanel');
     } else{
         console.log("Nouveau calcul : "+req.body.code);
-        amqp.connect("amqp://djnrmdtt:ld9_Znh1jijsXE3m9s8QC0U3V4bNkzw4@spotted-monkey.rmq.cloudamqp.com/djnrmdtt"   ,
+        amqp.connect("amqp://localhost/",
             function(err, conn){
                 if (err != null){
                     console.log("Connection error");
                     console.error(err);
                     process.exit(1);
                 }
-                    conn.createChannel(function(err, ch) {
-                        if (err != null){
-                            console.log("Channel creation error");
-                            console.error(err);
-                            process.exit(1);
-                        }
-                        var q='Work_queue';
-                        ch.assertQueue(q, {durable: false});
+                conn.createChannel(function(err, ch) {
+                    if (err != null){
+                        console.log("Channel creation error");
+                        console.error(err);
+                        process.exit(1);
+                    }
+                    else if(ch != null) {
+                        var QueueW = 'Work_queue';
+                        ch.assertQueue(QueueW, {durable: false});
                         //console.log(req.body.nbrJob);
-                        for(i=0;i<req.body.nbrJob;i++){
-                            ch.sendToQueue(q, new Buffer('Hello World!'+i));
-                            console.log(" [x] Sent 'Hello World!'");
+                        for (i = 0; i < req.body.nbrJob; i++) {
+                                console.log(" Job "+i+" --> creating ticket");
+                                ch.sendToQueue(QueueW, new Buffer(req.body.code), function () {
+                                    console.log(" Job "+i+" --> ticket is sent");
+                                });
                         }
-
-                    });
-                conn.close();
+                    }
+                });
         });
-
         res.render('calcpanel');
     }
     //console.log(request.session);
